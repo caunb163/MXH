7fbeef4e236bc7d9c07b87b4642d27eb16075c51
caunb <nguyenbacau163@gmail.com>
2021-02-22 16:43:44 +0700

    post with multiple image

--- app/src/main/java/com/caunb163/mxh/ui/main/home/HomeAdapter.kt
+++ app/src/main/java/com/caunb163/mxh/ui/main/home/HomeAdapter.kt
@@ -1,18 +1,26 @@
 package com.caunb163.mxh.ui.main.home
 
 import android.annotation.SuppressLint
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.request.RequestOptions
 import com.caunb163.domain.model.Post
 import com.caunb163.domain.model.User
 import com.caunb163.mxh.R
 import de.hdodenhof.circleimageview.CircleImageView
 
-class HomeAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class HomeAdapter(
+    private val glide: RequestManager
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
     private val TYPE_CREATE_POST = 0
     private val TYPE_POST = 1
 
@@ -39,9 +47,9 @@ class HomeAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         if (holder.itemViewType == TYPE_POST) {
-            (holder as HomeViewHolder).bind(list[position] as Post)
+            (holder as HomeViewHolder).bind(glide, list[position] as Post)
         } else {
-            (holder as CreatePostViewHolder).bind(list[position] as User)
+            (holder as CreatePostViewHolder).bind(glide, list[position] as User)
         }
     }
 
@@ -64,19 +72,134 @@ class HomeAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         private var comment: LinearLayout = view.findViewById(R.id.post_ll_cmt)
         private var share: LinearLayout = view.findViewById(R.id.post_ll_share)
 
+        private var ctl1: ConstraintLayout = view.findViewById(R.id.post_ctl1)
+        private var ctl2: ConstraintLayout = view.findViewById(R.id.post_ctl2)
+        private var ctl3: ConstraintLayout = view.findViewById(R.id.post_ctl3)
+        private var ctl4: ConstraintLayout = view.findViewById(R.id.post_ctl4)
+        private var ctl5: ConstraintLayout = view.findViewById(R.id.post_ctl5)
+
+        private var img1: ImageView = view.findViewById(R.id.post_1_img)
+
+        private var img21: ImageView = view.findViewById(R.id.post_2_img1)
+        private var img22: ImageView = view.findViewById(R.id.post_2_img2)
+
+        private var img31: ImageView = view.findViewById(R.id.post_3_img1)
+        private var img32: ImageView = view.findViewById(R.id.post_3_img2)
+        private var img33: ImageView = view.findViewById(R.id.post_3_img3)
+
+        private var img41: ImageView = view.findViewById(R.id.post_4_img1)
+        private var img42: ImageView = view.findViewById(R.id.post_4_img2)
+        private var img43: ImageView = view.findViewById(R.id.post_4_img3)
+        private var img44: ImageView = view.findViewById(R.id.post_4_img4)
+
+        private var img51: ImageView = view.findViewById(R.id.post_5_img1)
+        private var img52: ImageView = view.findViewById(R.id.post_5_img2)
+        private var img53: ImageView = view.findViewById(R.id.post_5_img3)
+        private var img54: ImageView = view.findViewById(R.id.post_5_img4)
+        private var img55: ImageView = view.findViewById(R.id.post_5_img5)
+        private var tvImgNumber: TextView = view.findViewById(R.id.post_tv_img_number)
+
         @SuppressLint("SetTextI18n")
-        fun bind(post: Post) {
+        fun bind(glide: RequestManager, post: Post) {
             username.text = post.userName
             createDate.text = post.createDate
             content.text = post.content
             likeNumber.text = post.likeNumber.toString()
             commentNumber.text = "${post.commentNumber} bình luận"
+
+            glide.applyDefaultRequestOptions(RequestOptions()).load(post.userAvatar).into(imgAvatar)
+            Log.e("TAG", "bind: ${post.images.size}")
+            when (post.images.size) {
+                1 -> {
+                    showImage(1)
+                    glide.applyDefaultRequestOptions(RequestOptions()).load(post.images[0])
+                        .into(img1)
+                }
+
+                2 -> {
+                    showImage(2)
+                    glide.applyDefaultRequestOptions(RequestOptions()).load(post.images[0])
+                        .into(img21)
+                    glide.applyDefaultRequestOptions(RequestOptions()).load(post.images[1])
+                        .into(img22)
+                }
+
+                3 -> {
+                    showImage(3)
+                    glide.applyDefaultRequestOptions(RequestOptions()).load(post.images[0])
+                        .into(img31)
+                    glide.applyDefaultRequestOptions(RequestOptions()).load(post.images[1])
+                        .into(img32)
+                    glide.applyDefaultRequestOptions(RequestOptions()).load(post.images[2])
+                        .into(img33)
+                }
+
+                4 -> {
+                    showImage(4)
+                    glide.applyDefaultRequestOptions(RequestOptions()).load(post.images[0])
+                        .into(img41)
+                    glide.applyDefaultRequestOptions(RequestOptions()).load(post.images[1])
+                        .into(img42)
+                    glide.applyDefaultRequestOptions(RequestOptions()).load(post.images[2])
+                        .into(img43)
+                    glide.applyDefaultRequestOptions(RequestOptions()).load(post.images[3])
+                        .into(img44)
+                }
+
+                5 -> {
+                    showImage(5)
+                    glide.applyDefaultRequestOptions(RequestOptions()).load(post.images[0])
+                        .into(img51)
+                    glide.applyDefaultRequestOptions(RequestOptions()).load(post.images[1])
+                        .into(img52)
+                    glide.applyDefaultRequestOptions(RequestOptions()).load(post.images[2])
+                        .into(img53)
+                    glide.applyDefaultRequestOptions(RequestOptions()).load(post.images[3])
+                        .into(img54)
+                    glide.applyDefaultRequestOptions(RequestOptions()).load(post.images[4])
+                        .into(img55)
+                    tvImgNumber.text = "+${post.images.size - 5}"
+                }
+                else -> {
+                    showImage(0)
+                }
+            }
+        }
+
+        fun showImage(size: Int) {
+            ctl1.visibility = View.GONE
+            ctl2.visibility = View.GONE
+            ctl3.visibility = View.GONE
+            ctl4.visibility = View.GONE
+            ctl5.visibility = View.GONE
+
+            when (size) {
+                1 -> {
+                    ctl1.visibility = View.VISIBLE
+                }
+                2 -> {
+                    ctl2.visibility = View.VISIBLE
+                }
+                3 -> {
+                    ctl3.visibility = View.VISIBLE
+                }
+                4 -> {
+                    ctl4.visibility = View.VISIBLE
+                }
+                5 -> {
+                    ctl5.visibility = View.VISIBLE
+                }
+                else -> {
+                }
+            }
         }
     }
 
     class CreatePostViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private var imgAvatar: CircleImageView = view.findViewById(R.id.createpost_avatar)
 
-        fun bind(user: User) {}
+        fun bind(glide: RequestManager, user: User) {
+            glide.applyDefaultRequestOptions(RequestOptions()).load(user.photoUrl).into(imgAvatar)
+        }
     }
 }
--- app/src/main/java/com/caunb163/mxh/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/caunb163/mxh/ui/main/home/HomeFragment.kt
@@ -5,6 +5,9 @@ import android.view.View
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.request.RequestOptions
 import com.caunb163.data.datalocal.LocalStorage
 import com.caunb163.domain.model.Post
 import com.caunb163.mxh.R
@@ -17,21 +20,31 @@ class HomeFragment : BaseFragment(R.layout.fragment_home) {
     private val TAG = "HomeFragment"
 
     private lateinit var recyclerView: RecyclerView
-
     private val viewModel: HomeViewModel by inject()
-    private val homeAdapter: HomeAdapter = HomeAdapter()
-    private val list = mutableListOf<Any>()
     private val localStorage: LocalStorage by inject()
+    private val list = mutableListOf<Any>()
+    private lateinit var glide: RequestManager
+    private lateinit var homeAdapter: HomeAdapter
 
     override fun initView(view: View) {
         recyclerView = view.findViewById(R.id.home_recycleview)
-    }
 
-    override fun initListener() {
+        glide = Glide.with(this)
+        glide.applyDefaultRequestOptions(
+            RequestOptions()
+                .placeholder(R.drawable.bg1)
+                .error(R.drawable.bg1)
+        )
+
+        homeAdapter = HomeAdapter(glide)
+
         recyclerView.apply {
             layoutManager = LinearLayoutManager(activity)
             adapter = homeAdapter
         }
+    }
+
+    override fun initListener() {
     }
 
     override fun initObserve() {
